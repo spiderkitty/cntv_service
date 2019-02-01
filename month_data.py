0eb097a76ef77471c2729ee77c0ec897729e4363
SuJiKiNen <luishen@vip.qq.com>
2019-02-01 19:24:28 +0800

    Add an option to to select program

--- month_data.py
+++ month_data.py
@@ -9,7 +9,8 @@ import papermill as pm
 @click.option("--end_date", help="end date.")
 @click.option("--year", help="year.", type=click.INT)
 @click.option("--month", help="month.", type=click.INT)
-def get_urls(start_date, end_date, year, month):
+@click.option("--program", help="program", type=click.INT)
+def get_urls(start_date, end_date, year, month, program):
     if not start_date:
         start_date = datetime.date(year, month, day=1).strftime("%Y%m%d")
     if not end_date:
@@ -21,14 +22,20 @@ def get_urls(start_date, end_date, year, month):
         end_date = datetime.date(year, month, day=1) - datetime.timedelta(days=1)
         end_date = end_date.strftime("%Y%m%d")
 
+    program_map = {
+        1: 'TOPC1451557646802924',  # 健康之路
+        2: 'TOPC1451546588784893',  # 生活圈
+    }
+
     pm.execute_notebook(
         './cntv_program_month_data.ipynb',
         './output.ipynb',
-        parameters = {
+        parameters={
             'year': year,
             'month': month,
             'start_date': start_date,
             'end_date': end_date,
+            'program': program_map[program],
         }
     )
     nb = pm.read_notebook('output.ipynb')
